4dee6b233ba12c1e42e088debb71d11e092ee66d
Steve Alvarado <33636552+Stevealv121@users.noreply.github.com>
2025-03-25 16:45:06 -0600

    Created operations board
    Implemented the operations board for the ALU. First segment displays a, second segment displays b and the third segment refers to the result. Segment, meaning 7 segment.

--- lab_2/alu4bits/src/top_module.sv
+++ lab_2/alu4bits/src/top_module.sv
@@ -1,11 +1,26 @@
 module top_module(
     input logic [9:0] SW,
-    output logic [6:0] HEX0,
+    output logic [6:0] HEX0,    // First operand display
+    output logic [6:0] HEX1,    // Second operand display
+    output logic [6:0] HEX2     // Result display
 );
 
-	 bin_to_bcd_decoder uut(
+    // First operand display (SW[3:0])
+    bin_to_bcd_decoder operand1_decoder(
         .bin_number(SW[3:0]),
         .bcd_number(HEX0)
-	 );
+    );
+    
+    // Second operand display (SW[7:4])
+    bin_to_bcd_decoder operand2_decoder(
+        .bin_number(SW[7:4]),
+        .bcd_number(HEX1)
+    );
+    
+    // Result display
+    bin_to_bcd_decoder result_decoder(
+        .bin_number(4'b0000),  //zero test
+        .bcd_number(HEX2)
+    );
 	
 endmodule
